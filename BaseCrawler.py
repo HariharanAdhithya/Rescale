3f0797302ccb6f69e0ffdbeae70a7de69906f5cd
HariharanAdhithya <hvenkat4@asu.edu>
2019-07-04 01:07:32 -0700

    Thread initial func

--- BaseCrawler.py
+++ BaseCrawler.py
@@ -1,13 +1,16 @@
 import urllib2
+import requests
 from bs4 import SoupStrainer
 from bs4 import BeautifulSoup
 
 # specify the url
 base_url = "http://www.rescale.com/"
 
-base_page = urllib2.urlopen(base_url)
+# base_page = urllib2.urlopen(base_url)
 
+res = requests.get(base_url)
 
+base_page = res.text
 only_a_tags = SoupStrainer('a')   # Filter and get only the anchor tags
 
 # parse the html using beautiful soup and store in variable `soup`
@@ -15,5 +18,5 @@ soup = BeautifulSoup(base_page, "html.parser", parse_only=only_a_tags)
 
 # Iterate over the filtered tags (only anchors)
 for anchors in soup:
-    if anchors.has_attr('href'):
+    if anchors.has_attr('href') and anchors['href'].startswith('http'):
         print(anchors['href'])
--- ThreadCrawler.py
+++ ThreadCrawler.py
@@ -1,5 +1,8 @@
+import threading
 from Queue import Queue
 
+import requests
+from bs4 import SoupStrainer, BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 
 
@@ -9,9 +12,10 @@ class ThreadCrawler:
         self.wait_queue = Queue()
         self.initial_url = input_url
         self.max_threads = max_threads
-        self.threads = ThreadPoolExecutor(max_workers = self.max_threads)
+        self.thread_executor = ThreadPoolExecutor(max_workers = self.max_threads)
         self.crawled_pages = set()
         self.enqueue(self.initial_url)
+        self.print_lock = threading.Lock()
 
     def enqueue(self, url):
         self.wait_queue.put(url)
@@ -19,3 +23,32 @@ class ThreadCrawler:
     def dequeue(self):
         return self.wait_queue.get()
 
+    def scrap(self, url):
+        res = requests.get(url)
+
+        base_page = res.text
+        only_a_tags = SoupStrainer('a')  # Filter and get only the anchor tags
+
+        # parse the html using beautiful soup and store in variable `soup`
+        soup = BeautifulSoup(base_page, "html.parser", parse_only=only_a_tags)
+
+        temp_set = set()
+
+        # Iterate over the filtered tags (only anchors)
+        for anchors in soup:
+            if anchors.has_attr('href') and anchors['href'].startswith('http'):
+                if anchors['href'] not in temp_set:
+                    temp_set.add(anchors['href'])
+                    self.enqueue(anchors['href'])
+        print(url)
+
+        for i in temp_set:
+            print('\t' + i)
+
+
+    def crawl(self):
+        cur_url = self.dequeue()
+        if cur_url not in self.crawled_pages:
+            self.crawled_pages.add(cur_url)
+            task = self.thread_executor.submit(self.scrap(cur_url))
+            
