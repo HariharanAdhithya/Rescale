7129c3e988834bf9d964c2306c48943a99a83639
HariharanAdhithya <hvenkat4@asu.edu>
2019-07-04 19:19:48 -0700

    Thread safe

--- ThreadCrawler.py
+++ ThreadCrawler.py
@@ -1,22 +1,19 @@
+from Queue import Queue, Empty
 import threading
-from Queue import Empty
-from multiprocessing import Queue
-
 import requests
-from bs4 import SoupStrainer, BeautifulSoup
-from concurrent.futures import ThreadPoolExecutor, as_completed, wait, ALL_COMPLETED
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 
 
 class ThreadCrawler:
 
     def __init__(self, input_url, max_threads):
-        self.wait_queue = []
+        self.wait_queue = Queue()
         self.initial_url = input_url
-        self.max_threads = max_threads
-        self.thread_executor = ThreadPoolExecutor(max_workers = 20)
+        self.thread_executor = ThreadPoolExecutor(max_workers = max_threads)
         self.crawled_pages = set()
-        self.wait_queue.append(self.initial_url)
-        self.print_lock = threading.Lock()
+        self.wait_queue.put(self.initial_url)
+        self.check_lock = threading.Lock()
 
 
     def scrape_info(self, url):
@@ -26,33 +23,27 @@ class ThreadCrawler:
         # parse the html using beautiful soup and store in variable `soup`
         soup = BeautifulSoup(html, "html.parser")
         href_links = soup.find_all('a', href=True)
-        temp_set = set()
         # Iterate over the filtered tags (only anchors)
+
+        self.check_lock.acquire()
+        print (url)
         for anchors in href_links:
             if anchors['href'].startswith('http') and anchors['href'] not in self.crawled_pages:
-                    temp_set.add(anchors['href'])
-                    self.wait_queue.append(anchors['href'])
+                    self.wait_queue.put(anchors['href'])
+                    self.crawled_pages.add(anchors['href'])
+                    print('\t' + anchors['href'])
+        self.check_lock.release()
 
-        self.print_lock.acquire()
-        print (url)
-        for i in temp_set:
-            print('\t' + i)
-        self.print_lock.release()
-
-        if len(self.wait_queue) != 0:
-            while self.wait_queue:
-                cur_url = self.wait_queue.pop(0)
-                if cur_url not in self.crawled_pages:
-                    self.crawled_pages.add(cur_url)
-                    self.thread_executor.submit(self.scrape_info, cur_url)
+        while not self.wait_queue.empty():
+            cur_url = self.wait_queue.get()
+            self.thread_executor.submit(self.scrape_info, cur_url)
 
 
     def crawl(self):
         try:
-            cur_url = self.wait_queue.pop(0)
-            if cur_url not in self.crawled_pages:
-                self.crawled_pages.add(cur_url)
-                self.thread_executor.submit(self.scrape_info, cur_url)
+            cur_url = self.wait_queue.get()
+            self.crawled_pages.add(cur_url)
+            self.thread_executor.submit(self.scrape_info, cur_url)
         except Empty:
             return
         except Exception as e:
