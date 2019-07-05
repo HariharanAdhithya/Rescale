a970094e22e9f1ef548d6d17bab818985e73c6a0
HariharanAdhithya <hvenkat4@asu.edu>
2019-07-04 22:37:35 -0700

    Cleaned without comments

--- ThreadCrawler.py
+++ ThreadCrawler.py
@@ -7,44 +7,49 @@ from concurrent.futures import ThreadPoolExecutor
 
 class ThreadCrawler:
 
-    def __init__(self, input_url, max_threads):
+    def __init__(self, max_threads):
         self.wait_queue = Queue()
-        self.initial_url = input_url
         self.thread_executor = ThreadPoolExecutor(max_workers = max_threads)
         self.crawled_pages = set()
-        self.wait_queue.put(self.initial_url)
         self.check_lock = threading.Lock()
 
-
-    def scrape_info(self, url):
-        res = requests.get(url, timeout=(3, 30))
-        html = res.text
-
+    def scrape_links(self, url, html):
         # parse the html using beautiful soup and store in variable `soup`
         soup = BeautifulSoup(html, "html.parser")
         href_links = soup.find_all('a', href=True)
-        # Iterate over the filtered tags (only anchors)
 
-        self.check_lock.acquire()
-        print (url)
-        for anchors in href_links:
-            if anchors['href'].startswith('http') and anchors['href'] not in self.crawled_pages:
-                    self.wait_queue.put(anchors['href'])
-                    self.crawled_pages.add(anchors['href'])
+        with self.check_lock:
+            print (url)
+            for anchors in href_links:
+                if anchors['href'].startswith('http'):
+                    if anchors['href'] not in self.crawled_pages:
+                        self.wait_queue.put(anchors['href'])
+                        self.crawled_pages.add(anchors['href'])
                     print('\t' + anchors['href'])
-        self.check_lock.release()
+
 
         while not self.wait_queue.empty():
             cur_url = self.wait_queue.get()
-            self.thread_executor.submit(self.scrape_info, cur_url)
+            self.thread_executor.submit(self.get_page, cur_url)
 
-
-    def crawl(self):
+    def get_page(self, url):
         try:
-            cur_url = self.wait_queue.get()
-            self.crawled_pages.add(cur_url)
-            self.thread_executor.submit(self.scrape_info, cur_url)
+            res = requests.get(url, timeout=(3, 30))
+        except requests.RequestException as e:
+            print(e)
+            return
+
+        if res and res.status_code == 200:
+            self.scrape_links(url, res.text)
+
+
+
+    def crawl(self, initial_url):
+        try:
+            self.crawled_pages.add(initial_url)
+            self.thread_executor.submit(self.get_page, initial_url)
         except Empty:
+            print('Queue is empty')
             return
         except Exception as e:
             print(e)
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from ThreadCrawler import ThreadCrawler
 
 
 # base_url = "http://triplebyte.github.io/web-crawler-test-site/already-passing-tests/"
+#
 base_url = "http://www.rescale.com/"
 
-job = ThreadCrawler(base_url, 100)
+job = ThreadCrawler(100)
 
-job.crawl()
+job.crawl(base_url)
